00fef24ece86b4b2507a064a5e988c9865c40781
srimaan <srimaansridharan@gmail.com>
2025-04-23 03:40:53 -0400

    Initial Commit

---
+++ UltraHealth/UltraHealthApp.swift
@@ -0,0 +1,17 @@
+//
+//  UltraHealthApp.swift
+//  UltraHealth
+//
+//  Created by Srimaan Sridharan on 4/23/25.
+//
+
+import SwiftUI
+
+@main
+struct UltraHealthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
